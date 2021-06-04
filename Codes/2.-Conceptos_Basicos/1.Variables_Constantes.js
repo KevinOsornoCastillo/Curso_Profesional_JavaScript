01bf5e42d7cab33572cae48c378424bbdb35c4c3
Kevin Osorno Castillo <kevinosorno220486@correo.itm.edu.co>
2021-06-04 14:28:53 -0500

    Seccion Conceptos Basicos

---
+++ Codes/2.-Conceptos_Basicos/1.Variables_Constantes.js
@@ -0,0 +1,11 @@
+// Declaracion de variables
+let nombre = "Kevin";
+var apellido = "Osorno Castillo";
+
+//Declaracion y asignación de constantes:
+const PI = 3.1416;
+
+//Declaracion dinamica
+let a = "Cadena";
+a = 23;
+a = 2.2;
